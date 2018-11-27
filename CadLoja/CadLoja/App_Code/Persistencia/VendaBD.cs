d6e672e22fc5bf18a9b43ea80a2234b66b79a8e0
shield5 <elsom.junr@gmail.com>
2018-11-27 02:09:44 -0200

    classe de persistencia VendaBD com método SelectAll() criada. na pagina ListarVenda é exibida o total vendido em um mes especifico. exibe também os produtos vendidos e o numero de produtos vendidos naquele mês.

---
+++ CadLoja/CadLoja/App_Code/Persistencia/VendaBD.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using FATEC;
+using System.Data;
+
+namespace CadLoja.App_Code.Persistencia
+{
+    public class VendaBD
+    {
+        //selectall
+        public DataSet SelectAll()
+        {
+            DataSet ds = new DataSet();
+
+            System.Data.IDbConnection objConexao;
+            System.Data.IDbCommand objCommand;
+            System.Data.IDataAdapter objDataAdapter;
+
+            objConexao = Mapped.Connection();
+            objCommand = Mapped.Command("select year(ven_data) as ano, month(ven_data) as mes, group_concat(pl_tipo order by pl_tipo separator ', ') as produtos, count(i.ven_codigo) as vendidos, sum(p.pl_preco) as total from tbl_vendas v inner join tbl_itensvenda i using (ven_codigo) inner join tbl_produtoloja p using (pl_codigo) group by year(ven_data), month(ven_data) order by ano desc, mes desc", objConexao);
+            objDataAdapter = Mapped.Adapter(objCommand);
+            objDataAdapter.Fill(ds);
+
+            objConexao.Close();
+
+            objCommand.Dispose();
+            objConexao.Dispose();
+
+            return ds;
+        }
+    }
+}
--- CadLoja/CadLoja/Paginas/ListarVenda.aspx.cs
+++ CadLoja/CadLoja/Paginas/ListarVenda.aspx.cs
@@ -4,14 +4,24 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Data;
+using CadLoja.App_Code.Persistencia;
 
 namespace CadLoja.Paginas
 {
     public partial class ListarVenda : System.Web.UI.Page
     {
+        private void Carrega()
+        {
+            VendaBD bd = new VendaBD();
+            DataSet ds = bd.SelectAll();
+            GridView1.DataSource = ds.Tables[0].DefaultView;
+            GridView1.DataBind();
+        }
+
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            Carrega();
         }
     }
 }
